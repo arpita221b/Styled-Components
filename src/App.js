312cb21d5ec137b05315de42eb33d8e8bcafdd6d
Arpita Naval <navalarpita@gmail.com>
2021-12-07 21:46:33 +0530

    Change styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import {Container} from './Components/Container.styled'
+import { ThemeProvider } from 'styled-components';
+import {Container} from './Components/Container.styled';
+import Header from "./Components/Header";
+import GlobalStyles from './Components/Global';
+
+const theme = { 
+  colours: {
+    header: '#ebfbff',
+    body:'#fff',
+    footer:'#003333'
+  }
+}
 function App() {
   return (
+    <ThemeProvider theme = {theme}>
+       <>
+       <GlobalStyles/>
+    <Header/>
    <Container>
-   <h1>
-   Hello World
-   </h1>
-
+   <h1> Hello World</h1>
    </Container>
 
-
+</>
+    </ThemeProvider>
+   
   );
 }
 
---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyledHeader } from './Styles/Header.styles'
+
+export default function Header() {
+    return (
+        <StyledHeader>
+        <h1>DevCommunity</h1>
+        </StyledHeader>
+       
+       
+    )
+}
